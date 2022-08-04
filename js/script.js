438b592b8ac1447a9a4c16405e35b2bab529d785
silviordjr <silviordjr@outlook.com>
2022-08-04 17:17:16 -0300

    slide quartos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 import initMenuMobile from './modules/menuMobile.js';
 import { initAnimacaoScroll } from './modules/scroll.js';
+import {SlideNav} from "./modules/slide.js";
+
+const slide = new SlideNav ('.slide', '.wrapper')
+
+const hostel = document.querySelector("#hostel-services")
+
+if (hostel){
+    slide.init()
+    slide.onResize(true)
+    slide.addControl('.custom-control')
+    slide.addTimeChange(10000)
+}
 
 
 initMenuMobile()
