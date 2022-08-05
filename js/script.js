d1f81590e7cfd9ca3b4cdc580b4c80bde9c192b9
silviordjr <silviordjr@outlook.com>
2022-08-05 09:02:07 -0300

    camping e eventos

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ import {SlideNav} from "./modules/slide.js";
 const slide = new SlideNav ('.slide', '.wrapper')
 
 const hostel = document.querySelector("#hostel-services")
+const camping = document.querySelector('#camping')
+const eventos = document.querySelector('#eventos')
 
-if (hostel){
+if (hostel || camping || eventos){
     slide.init()
     slide.onResize(true)
     slide.addControl('.custom-control')
     slide.addTimeChange(10000)
 }
-
 
 initMenuMobile()
 initAnimacaoScroll()
