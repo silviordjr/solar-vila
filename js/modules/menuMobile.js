2e7b17c41dc05235f9772904ef6776804724b33a
silviordjr <silviordjr@outlook.com>
2022-08-11 14:44:31 -0300

    automacoes

--- js/modules/menuMobile.js
+++ js/modules/menuMobile.js
@@ -1,19 +1,17 @@
-import outsideClick from './outsideClick.js';    
+import outsideClick from './outsideClick.js';
 
-export default function initMenuMobile () {
+export default function initMenuMobile() {}
 
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+
+function openMenu() {
+  menuList.classList.add('ativo');
+  menuButton.classList.add('ativo');
+  outsideClick(menuList, () => {
+    menuList.classList.remove('ativo');
+    menuButton.classList.remove('ativo');
+  });
 }
 
-const menuButton = document.querySelector('[data-menu="button"]')
-const menuList = document.querySelector('[data-menu="list"]')
-
-function openMenu (event) {
-    menuList.classList.add('ativo')
-    menuButton.classList.add('ativo')
-    outsideClick(menuList, () => {
-        menuList.classList.remove('ativo')
-        menuButton.classList.remove('ativo')
-    })
-}
-
-menuButton.addEventListener('click', openMenu)
+menuButton.addEventListener('click', openMenu);
